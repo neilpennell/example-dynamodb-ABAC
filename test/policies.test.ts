32d406a800ac0fbee26a1d2ec45359cedd2ec125
Neil Pennell <neil.pennell@gmail.com>
2025-01-08 17:15:01 -0500

    Initial commit

---
+++ test/policies.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Policies from '../lib/policies-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/policies-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Policies.PoliciesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
